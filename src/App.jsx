d1cf9c63de87a5340fda5e83fec5f4df1996557e
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-08-24 10:43:01 -0400

    new post request

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 import { useEffect, useState } from "react";
 import TodoList from "./TodoList.jsx";
 import  AddTodoForm from "./AddTodoForm.jsx";
+import {Routes, Route, Link} from 'react-router-dom';
+// import axios from 'axios';
 
 
 function App() {
 
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
+
+  //calls this addCat 
+  async function addData(todoTitle){
+    const newTodoData = {
+      title: todoTile
+    }
+
+    const options = {
+        method: "POST",
+        headers: {
+          Authorization:`Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
+        },
+        body: JSON.stringify(newTodoData)
+    };
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+    try{
+      const response = await fetch(url, options);
+        if(!response.ok){
+          throw new Error(`${response.status}`);
+        }
+        const data = await response.json();
+        console.log('your data', data);
+    }
+    catch(error){
+      console.log(error.message)
+      return null;
+    }
+  }
+
 
   async function fetchData(){
     const options = {
@@ -65,14 +96,28 @@ function App() {
 
   return (
     <>
-      <div id='main'>
-      <section id='app-content'>
-      <h1>TODO APP</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      {isLoading ? (<p>Loading....</p>) 
-      : (<TodoList onRemoveTodo={removeTodo} todoList={todoList} />)}
-      </section>
+    <nav>
+      <div>logo</div>
+      <div>
+        <ul>
+          <li><Link to="/">home</Link></li>
+          <li><Link to="/new">home</Link></li>
+        </ul>
       </div>
+    </nav>
+    <Routes>
+    <Route path="/" element={
+         <div id='main'>
+         <section id='app-content'>
+         <h1>TODO APP</h1>
+         <AddTodoForm onAddTodo={addTodo} />
+         {isLoading ? (<p>Loading....</p>) 
+         : (<TodoList onRemoveTodo={removeTodo} todoList={todoList} />)}
+         </section>
+         </div>
+    }/>
+    <Route path='/new'><h1>New Todo List</h1></Route>
+      </Routes>
     </>
   );
 }
