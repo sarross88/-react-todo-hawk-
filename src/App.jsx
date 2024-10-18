c39e4fd3e84cd5dd21053d50d801ce7f09707d44
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-10-18 13:25:07 -0400

    oct 18

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import TodoList from "./TodoList.jsx";
 import AddTodoForm from "./AddTodoForm.jsx";
-import ToggleSwitch from './ToggleSwitch.jsx'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import ToggleSwitch from "./ToggleSwitch.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const BASE_URL = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`
+const BASE_URL = `https://api.airtable.com/v0/${
+  import.meta.env.VITE_AIRTABLE_BASE_ID
+}/${import.meta.env.VITE_TABLE_NAME}`;
 const TOKEN = import.meta.env.VITE_AIRTABLE_API_TOKEN;
-
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -47,49 +48,47 @@ function App() {
     }
   }
 
-//add data to airtable
-async function addTodo(newTodoTitle) {
-  const options = {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      records: [
-        {
-          fields: {
-            title: newTodoTitle,
+  //add data to airtable
+  async function addTodo(newTodoTitle) {
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        records: [
+          {
+            fields: {
+              title: newTodoTitle,
+            },
           },
-        },
-      ],
-    }),
-  };
-
-  try {
-    const response = await fetch(BASE_URL, options);
-
-    if (!response.ok) {
-      throw new Error(`${response.status}`);
-    }
-
-    const data = await response.json();
-
-    const newTodo = {
-      title: data.records[0].fields.title,
-      id: data.records[0].id,
+        ],
+      }),
     };
 
-    setTodoList((prevTodoList) => [newTodo, ...prevTodoList]);
-  } catch (error) {
-    console.log(error.message);
-    return null;
+    try {
+      const response = await fetch(BASE_URL, options);
+
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      const data = await response.json();
+
+      const newTodo = {
+        title: data.records[0].fields.title,
+        id: data.records[0].id,
+      };
+
+      setTodoList((prevTodoList) => [newTodo, ...prevTodoList]);
+    } catch (error) {
+      console.log(error.message);
+      return null;
+    }
   }
-}
 
-
-
-  //this function changes the state 
+  //this function changes the state
   async function fetchData() {
     const options = {
       method: "GET",
@@ -100,10 +99,12 @@ async function addTodo(newTodoTitle) {
 
     try {
       // const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`
-      const query1 = '?view=Grid%20view&sort[0][field]=Title';
-      const query2= '&sort[0][field]=title';
-      const query3 = '&sort[0][direction]=asc';
-      const urlSpecial = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}${query1+query2+query3}`
+      const query1 = "?view=Grid%20view&sort[0][field]=Title";
+      const query2 = "&sort[0][field]=title";
+      const query3 = "&sort[0][direction]=asc";
+      const urlSpecial = `https://api.airtable.com/v0/${
+        import.meta.env.VITE_AIRTABLE_BASE_ID
+      }/${import.meta.env.VITE_TABLE_NAME}${query1 + query2 + query3}`;
       const response = await fetch(urlSpecial, options);
 
       if (!response.ok) {
@@ -124,32 +125,33 @@ async function addTodo(newTodoTitle) {
     }
   }
 
-  //empty array makes it only run once when app first runs, NO await needed 
+  //empty array makes it only run once when app first runs, NO await needed
   useEffect(() => {
     fetchData();
   }, []);
 
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={
-         <main>
-         <h1>Todos</h1>
-         <AddTodoForm onAddTodo={addTodo} />
-         {isLoading ? (
-           <p>Loading...</p>
-         ) : (
-          <>
-          <ToggleSwitch/>
-           <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
-           </>
-         )}
-       </main>
-      }
-    />
-    <Route path="/new" element={<h1>New Todo List</h1>}/>
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1>Todos</h1>
+              <AddTodoForm onAddTodo={addTodo} />
+              {isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                <>
+                  <ToggleSwitch />
+                  <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
+                </>
+              )}
+            </main>
+          }
+        />
+        <Route path="/new" element={<h1>New Todo List</h1>} />
+      </Routes>
     </BrowserRouter>
   );
 }
